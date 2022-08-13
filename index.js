5e678099ec17b1dd0c0a3d0f395a7e870341f60e
Allan Araújo Pinheiro <apinheiro@railz.ai>
2022-08-13 13:58:18 -0300

    Adicionado ternário

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// let isAdmin = false;
+// let isSabado = true;
+// !isAdmin && isSabado && console.log(`nao é admin`)
+
+// ternário
+let a = true;
+let b = true;
+
+if (a && b) {
+  console.log(1);
+  console.log(1);
+} else {
+  console.log(2);
+}
+
+a && b ? (a = false) : console.log(2);
+a && b ? (a = false) : console.log(2);
+
+console.log(a);
+
 let isAdmin = false;
-let isSabado = true;
-!isAdmin && isSabado && console.log(`nao é admin`) 
+!isAdmin && console.log(1);
+!isAdmin ? console.log(1) : null;
