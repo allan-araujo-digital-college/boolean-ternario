6238808ce3b7e9f764043db4ba7b51afa58c751d
Allan Araújo Pinheiro <apinheiro@railz.ai>
2022-08-13 16:08:21 -0300

    Break

--- index2.js
+++ index2.js
@@ -1,24 +1,22 @@
-// console.log(1);
-// console.log(2);
+// console.log(10);
+// console.log(9);
+// console.log(8);
+// console.log(7);
+// console.log(6);
+// console.log(5);
+// console.log(4);
 // console.log(3);
+// console.log(2);
 
-// for(inicialização; condição; passo){
-
+// for (let i = 10; i > 1; i = i - 1) {
+//   if(i === 3){
+//     console.log('o i ficou igual a 3')
+//   }
+//   console.log(i);
 // }
+// console.log('linha 18')
 
-// let a = 9;
-// let isNumeroPar = a % 2 === 0;
-// console.log(isNumeroPar);
-
-let chamadaDeAlunos = ["allan", "josé", "maria", "joão", "ana"];
-// console.log("length");
-// console.log(chamadaDeAlunos.length);
-
-// for (let i = 0; i < chamadaDeAlunos.length; i++) {
-//   console.log(chamadaDeAlunos[i]);
-// }
-
-for (let i = 0; i < chamadaDeAlunos.length; i++) {
-  let element = chamadaDeAlunos[i];
-  console.log(element)
+for (let i = 1000; i <= 5000; i = i + 100) {
+  console.log(i);
 }
+console.log("linha 18");
