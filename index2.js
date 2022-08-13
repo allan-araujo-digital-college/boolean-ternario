abbe84010cc87c1b060f215a64f22fbd801313c3
Allan Araújo Pinheiro <apinheiro@railz.ai>
2022-08-13 14:07:04 -0300

    i++ e ++i

---
+++ index2.js
@@ -0,0 +1,8 @@
+let variavelArrayNomes = ["allan", "josé", "renato", 99];
+console.log(variavelArrayNomes);
+let i = 0; // i = 0
+console.log(variavelArrayNomes[i]); // i = 0
+console.log(variavelArrayNomes[i++]); // i = 0
+console.log(variavelArrayNomes[i++]); // i = 1
+// i = 2
+console.log(variavelArrayNomes[++i]); // i = 3
