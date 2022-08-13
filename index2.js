dbe69f0e4213945eeefaee573084b5b8b199113f
Allan Araújo Pinheiro <apinheiro@railz.ai>
2022-08-13 15:01:23 -0300

    repeticao

--- index2.js
+++ index2.js
@@ -1,2 +1,24 @@
-let i = 0;
-console.log('explicando o for')
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// for(inicialização; condição; passo){
+
+// }
+
+// let a = 9;
+// let isNumeroPar = a % 2 === 0;
+// console.log(isNumeroPar);
+
+let chamadaDeAlunos = ["allan", "josé", "maria", "joão", "ana"];
+// console.log("length");
+// console.log(chamadaDeAlunos.length);
+
+// for (let i = 0; i < chamadaDeAlunos.length; i++) {
+//   console.log(chamadaDeAlunos[i]);
+// }
+
+for (let i = 0; i < chamadaDeAlunos.length; i++) {
+  let element = chamadaDeAlunos[i];
+  console.log(element)
+}
